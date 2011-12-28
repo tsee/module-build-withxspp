06a27ee5d3a877f4a63880b0764f89250393299f
Steffen Mueller <smueller@cpan.org>
2011-12-28 08:46:22 +0100

    Version bump and changelog

--- lib/Module/Build/WithXSpp.pm
+++ lib/Module/Build/WithXSpp.pm
@@ -6,7 +6,7 @@ use Module::Build;
 use ExtUtils::CppGuess ();
 
 our @ISA = qw(Module::Build);
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 # TODO
 # - configurable set of xsp and xspt files (and XS typemaps?)
